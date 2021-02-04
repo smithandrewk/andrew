4176758c2849ce78038df0ffe19724ab2a2d6055
Andrew <andrewsmith1025@gmail.com>
2021-02-04 14:10:10 -0500

    add packages to installer

--- .dotfiles/install.sh
+++ .dotfiles/install.sh
@@ -3,3 +3,11 @@ sudo apt install vim
 sudo apt install tmux
 sudo apt install git
 sudo apt install cowsay
+rm -rf Desktop/ Documents/ Downloads/ Music/ Pictures/ Public/ Templates/ Videos/
+sudo apt install gparted
+sudo apt install rclone
+sudo apt remove firefox
+sudo apt purge firefox
+sudo apt update
+sudo apt upgrade
+sudo apt autoremove
